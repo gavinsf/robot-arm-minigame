45aa78c366ad29c30490edf5077f172972517555
Jacob <winkschonewille@gmail.com>
2024-11-29 10:07:18 -0800

    created unique material system

--- bin/scripts/ball.gd
+++ bin/scripts/ball.gd
@@ -2,15 +2,13 @@ extends RigidBody3D
 
 
 @onready var ball_mesh : MeshInstance3D = $BallMeshInstance
-static var ball_materials : Array[StandardMaterial3D] = []
+static var ball_colours : Array[Color]  = [Color(1,0,0), Color(0,1,0), Color(0,0,1)]
 var ball_colour : int
 
 
 # Virtuals ###################################################################
-func _init() -> void:
-	ball_materials.append(load("res://materials/ball_material_0.tres"))
-	ball_materials.append(load("res://materials/ball_material_1.tres"))
-	ball_materials.append(load("res://materials/ball_material_2.tres"))
-
 func _ready() -> void:
-	ball_mesh.mesh.surface_set_material(0, ball_materials[ball_colour])
+	var _ball_material = StandardMaterial3D.new()
+	_ball_material.albedo_color = ball_colours[ball_colour]
+	
+	ball_mesh.set_surface_override_material(0,_ball_material)

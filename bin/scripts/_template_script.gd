b969991a6c4b569497d9dff37968f8909858abbb
Jacob <winkschonewille@gmail.com>
2024-11-30 19:47:39 -0800

    added collection bin

---
+++ bin/scripts/_template_script.gd
@@ -0,0 +1,19 @@
+extends Node
+#class_name 
+##
+
+#@export var classMember : Node = 0
+#@onready var nodeReferencedChild : Node = $Control
+#signal UserDefinedSignal
+
+
+# VIRTUALS ####################################################
+#func _init() -> void: pass
+#func _ready() -> void: pass
+#func _process(delta: float) -> void: pass
+
+
+
+# SIGNALS #####################################################
+#func _on_UserDefinedSignal(): pass
+# HELPERS #####################################################
---
+++ bin/scripts/play_bin.gd
@@ -0,0 +1,15 @@
+extends Node
+## Holds balls. Bottom can open as part of a reset mechanism
+
+#@export var classMember : Node = 0
+#@onready var nodeReferencedChild : Node = $Control
+#signal UserDefinedSignal
+
+# VIRTUALS ####################################################
+#func _init() -> void: pass
+#func _ready() -> void: pass
+#func _process(_delta: float) -> void: pass
+#func _physics_process(_delta: float) -> void: pass
+
+# SIGNALS #####################################################
+# HELPERS #####################################################
